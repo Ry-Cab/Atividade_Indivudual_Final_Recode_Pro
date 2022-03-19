22982efc87cf7a11ff8835fa66dfc94c366c896e
Ryan Cabral <cabral.reis23@gmail.com>
2022-03-18 21:38:12 -0300

    final

--- frontend/site_tripshow/tripshow-react/src/App.js
+++ frontend/site_tripshow/tripshow-react/src/App.js
@@ -8,9 +8,6 @@ import Contato from './Views/Contato.jsx'
 import Destinos from './Views/Destinos.jsx'
 import Promocoes from './Views/Promocoes';
 
-import DestinosTeste from './Views/DestinosTeste';
-import CreateDestino from './Views/CREATE/CreateDestino.jsx';
-import UpdateDestino from './Views/CREATE/UpdateDestino.jsx';
 import DestinoDetails from './Views/DestinoDetails.jsx';
 
 function App() {
@@ -30,10 +27,6 @@ function App() {
        <Route path="/Promocoes" component={Promocoes}/>
 
        <Route path="/Destinos-details/:id" exact component={DestinoDetails}/>
- 
-       <Route path="/DestinosTeste" component={DestinosTeste}/>
-       <Route path="/Destinos_CREATE" component={CreateDestino}/>
-       <Route path="/Destinos_UPDATE:id" component={UpdateDestino}/>
      </Switch>
  
       <Footer />
